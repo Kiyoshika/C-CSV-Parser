3309fa03e107076d298993e04d853c1a426b72c2
Kiyoshika <zachlweaver00@gmail.com>
2022-02-16 20:26:56 -0500

    csv_read() mostly finished. TODO: add quote support. csv_free() added to deallocate all the memory used to store all the data from CSV file. after adding quote support, will implement read by column functions and then casting functions to convert char* types to int or float.

--- include/csvparser.h
+++ include/csvparser.h
@@ -4,7 +4,87 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
-size_t csv_parse_line(const char* line, char*** tokens, char delim);
+/**
+ * Read a CSV file and store cells into a char*** pointer.
+ * @param filename Filename to read CSV file from.
+ * @param data A char*** that holds the CSV cells (need to pass by address, hence the parameter is char****). It's structured as data[x][y] where x represents the row, y represents the column and the contents is a string (char*).
+ * @param data_dims A size_t array with two indices: 0th index stores the row count and 1st index stores the column count.
+ * @param delim A single-character delimiter.
+ * @param has_headers A boolean indicating if the file has headers or not. If true, the first line will be skipped and not stored into data.
+ *
+ * Example:
+ * @code
+ * #include "csvparser.h"
+ *
+ * int main() {
+ *     char*** data = NULL;
+ *     size_t data_dims[2];
+ *
+ *     csv_read("../test.csv", &data, &data_dims, ',', true);
+ *
+ *     printf("Rows: %zu\nColumns: %zu\n\n", data_dims[0], data_dims[1]);
+ *
+ *     // print all cells of the parsed data
+ *     for (size_t i = 0; i < data_dims[0]; ++i)
+ *     {
+ *         for (size_t j = 0; j < data_dims[1]; ++j)
+ *         {
+ *             printf("%s ", data[i][j]);
+ *         }
+ *         printf("\n");
+ *     }
+ *
+ *     // don't forget to free all the memory allocated to store the strings
+ *     csv_free(&data, data_dims);
+ *
+ *     return 0;
+ * }
+ * @endcode
+ *
+ * Output:
+ *
+ * @code
+ * Rows: 3
+ * Columns: 3
+ *
+ * 10 joe 10.33
+ * 20 jim 41.55
+ * 30 kyle -3.55
+ * @endcode
+ */
+void csv_read(const char* filename, char**** data, size_t (*data_dims)[2], char delim, bool has_headers);
+
+/**
+ * Read a single column (by index) from CSV file and store cells into a char** pointer.
+ * @param filename Filename to read CSV file from.
+ * @param column_index Index of the column to read.
+ * @param data A char** that holds the CSV cells from the specified column
+ * @param delim A single-character delimiter.
+ * @param has_headers A boolean indicating if the file has headers or not. If true, the first line will be skipped and not stored into data.
+ *
+ */
+void csv_read_column_by_index(const char* filename, size_t column_index, char*** data, char delim, bool has_headers);
+
+/**
+ * Read a single column (by name) from CSV file and store cells into a char** pointer.
+ * @param filename Filename to read CSV file from.
+ * @param column_index Index of the column to read.
+ * @param data A char** that holds the CSV cells from the specified column
+ * @param delim A single-character delimiter.
+ *
+ */
+void csv_read_column_by_name(const char* filename, size_t column_index, char*** data, char delim);
+
+/**
+ * Free the memory allocated to data after reading a CSV file. This MUST be done if you intent on using the same pointer to read a different file.
+ * @param data The address to a char*** pointer holding the CSV data loaded by csv_read().
+ * @param data_dims Array of data dimensions with the 0th index counting the rows and 1st index counting the columns.
+ */
+void csv_free(char**** data, size_t data_dims[2]);
+
+// inactive for now
+void csv_free_column(char*** data, size_t rows);
 
 #endif //CSVPARSER_CSVPARSER_H
--- src/csvparser.c
+++ src/csvparser.c
@@ -14,6 +14,7 @@ size_t csv_count_columns(const char* line, char delim)
     return delim_count + 1; // + 1 since no delimiter at end of string
 }
 
+// internal function
 // parses a line of CSV and stores parsed values into tokens
 // tokens must be free'd by the client
 // TODO: add support for delimiters within quotes to avoid parsing issues
@@ -62,10 +63,98 @@ size_t csv_parse_line(const char* line, char*** tokens, char delim)
     // no delimiter at end of string so we check if parsed_value still contains content
     if (strlen(parsed_value) > 0)
     {
+        // remove trailing \n from input
+        parsed_value[strcspn(parsed_value, "\n")] = 0;
+
         (*tokens)[current_col] = strdup(parsed_value);
         free(parsed_value);
         parsed_value = NULL;
     }
 
     return delim_count;
+}
+
+void csv_read(const char* filename, char**** data, size_t (*data_dims)[2], char delim, bool has_headers)
+{
+    FILE* file = fopen(filename, "r");
+    if (file != NULL)
+    {
+        char* line = NULL;
+        char **tokens = NULL;
+        size_t len = 0;
+        size_t read = 0;
+
+        // start with allocating memory for 10 lines, then double each time capacity is reached
+        size_t row_allocation_size = 10;
+        (*data) = calloc(row_allocation_size, sizeof(char**));
+
+        size_t current_row = 0;
+        size_t n_tokens;
+
+        bool counted_columns = false;
+
+        while ((read = getline(&line, &len, file)) != -1)
+        {
+            // count the columns and store them into first index in data_dims
+            if (!counted_columns)
+            {
+                (*data_dims)[1] = csv_count_columns(line, delim);
+                counted_columns = true;
+            }
+
+            // skip header line if present
+            if (has_headers)
+            {
+                has_headers = false;
+                continue;
+            }
+
+            n_tokens = csv_parse_line(line, &tokens, delim);
+            (*data)[current_row] = malloc(sizeof(char*) * n_tokens);
+            for (size_t i = 0; i < n_tokens; ++i)
+            {
+                // duplicate a copy of tokens[i] into data then free it so we can use tokens on next iteration
+                (*data)[current_row][i] = strdup(tokens[i]);
+                free(tokens[i]);
+                tokens[i] = NULL;
+            }
+
+            current_row++;
+            if (current_row >= row_allocation_size)
+            {
+                row_allocation_size *= 2;
+                (*data) = realloc((*data), sizeof(char**) * row_allocation_size);
+            }
+
+            // free tokens to use them on next iteration
+            free(tokens);
+            tokens = NULL;
+        }
+        free(line);
+        fclose(file);
+
+        // store row count into 0th index in data_dims
+        (*data_dims)[0] = current_row;
+    }
+    else
+    {
+        printf("File not found!\n");
+        exit(-1);
+    }
+}
+
+void csv_free(char**** data, size_t data_dims[2])
+{
+    for (size_t i = 0; i < data_dims[0]; ++i)
+    {
+        for (size_t j = 0; j < data_dims[1]; ++j)
+        {
+            free((*data)[i][j]);
+            (*data)[i][j] = NULL;
+        }
+        free((*data)[i]);
+        (*data)[i] = NULL;
+    }
+    free((*data));
+    (*data) = NULL;
 }
